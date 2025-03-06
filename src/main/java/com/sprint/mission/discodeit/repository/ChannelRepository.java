7d9a00aeb3d223a30621088ed441ada2730c457e
Minhyuk Sim <minhyuk3954@gmail.com>
2025-03-06 16:37:13 +0900

    기본 요구사항 - 레포지토리 설계 및 구현 시작

---
+++ src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
@@ -0,0 +1,11 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.Channel;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ChannelRepository {
+
+
+}
