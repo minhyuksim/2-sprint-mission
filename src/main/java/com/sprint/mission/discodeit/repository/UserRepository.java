f86dbe610269926d42570e9e80cc3de467cba3e9
Minhyuk Sim <minhyuk3954@gmail.com>
2025-03-06 17:44:53 +0900

    기본 요구사항 - 레포지토리 설계 및 구현 시작 1. JCF 메세지, 유저 설계 및 구현

--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.sprint.mission.discodeit.repository;
 
-import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.User;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface UserRepository {
-
+    User save(User user);
+    User findById(UUID id);
+    List<User> findAll();
+    void update(User user);
+    void delete(UUID id);
 }
--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
@@ -1,4 +1,40 @@
 package com.sprint.mission.discodeit.repository.jcf;
 
-public class JCFMessageRepository {
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+
+import java.util.*;
+
+public class JCFMessageRepository implements MessageRepository {
+    private final Map<UUID, Message> data;
+
+    public JCFMessageRepository() {
+        this.data = new HashMap<>();
+    }
+
+    @Override
+    public Message save(Message message) {
+        data.put(message.getId(), message);
+        return message;
+    }
+
+    @Override
+    public Message findById(UUID id) {
+        return data.get(id);
+    }
+
+    @Override
+    public List<Message> findAll() {
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public void update(Message message) {
+        data.put(message.getId(), message);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        data.remove(id);
+    }
 }
