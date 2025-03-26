72bab294191b6f9b73dc0f7636c2b25f9655fe4c
Minhyuk Sim <minhyuk3954@gmail.com>
2025-03-26 16:10:26 +0900

    1. feat: MessageController SendMessage 담당 메소드 구현

---
+++ src/main/java/com/sprint/mission/discodeit/Controller/MessageController.java
@@ -0,0 +1,48 @@
+package com.sprint.mission.discodeit.Controller;
+
+import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
+import com.sprint.mission.discodeit.dto.request.MessageCreateRequest;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.service.MessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.print.attribute.standard.Media;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/message")
+@RequiredArgsConstructor
+public class MessageController {
+    private final MessageService messageService;
+
+    @PostMapping(value ="", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Message> sendMessage(@RequestPart("message") MessageCreateRequest messageCreateRequest,
+                                               @RequestPart(name="attachments", required = false) MultipartFile[] attachmentFiles) {
+        List<BinaryContentCreateRequest> attachments = new ArrayList<>();
+        if (attachmentFiles != null) {
+            for (MultipartFile file : attachmentFiles) {
+                if (!file.isEmpty()) {
+                    try {
+                        BinaryContentCreateRequest attachment = BinaryContentCreateRequest.builder()
+                                .fileName(file.getOriginalFilename())
+                                .contentType(file.getContentType())
+                                .bytes(file.getBytes())
+                                .build();
+                        attachments.add(attachment);
+                    }catch(IOException e) {
+                        throw new RuntimeException("Error creating attachment", e);
+                    }
+                }
+            }
+        }
+        Message createdMessage = messageService.create(messageCreateRequest, attachments);
+        return ResponseEntity.ok(createdMessage);
+
+    }
+}
