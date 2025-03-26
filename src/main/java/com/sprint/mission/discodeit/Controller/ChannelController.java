3ed755038227028f14a69b69a3233dfb34047593
Minhyuk Sim <minhyuk3954@gmail.com>
2025-03-26 14:16:05 +0900

    1. feat: ChannelController 채널 생성 담당 메소드 구현

---
+++ src/main/java/com/sprint/mission/discodeit/Controller/ChannelController.java
@@ -0,0 +1,35 @@
+package com.sprint.mission.discodeit.Controller;
+
+import com.sprint.mission.discodeit.dto.data.ChannelDto;
+import com.sprint.mission.discodeit.dto.request.PrivateChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.request.PublicChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.request.PublicChannelUpdateRequest;
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.service.ChannelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/channel")
+@RequiredArgsConstructor
+public class ChannelController {
+    private final ChannelService channelService;
+
+    @PostMapping(value = "/public", consumes = "application/json")
+    public ResponseEntity<ChannelDto> createPublicChannel(@RequestBody PublicChannelCreateRequest publicChannelCreateRequest){
+        Channel createdChannel = channelService.create(publicChannelCreateRequest);
+        ChannelDto channeldto = channelService.find(createdChannel.getId());
+        return ResponseEntity.ok(channeldto);
+    }
+
+    @PostMapping(value = "/private", consumes = "application/json")
+    public ResponseEntity<ChannelDto> createPrivateChannel(@RequestBody PrivateChannelCreateRequest privateChannelCreateRequest){
+        Channel createdChannel = channelService.create(privateChannelCreateRequest);
+        ChannelDto channeldto = channelService.find(createdChannel.getId());
+        return ResponseEntity.ok(channeldto);
+    }
+
+
