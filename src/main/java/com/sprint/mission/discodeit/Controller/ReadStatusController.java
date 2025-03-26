169875585a6b0bb4a7578ffacb834171192f3137
Minhyuk Sim <minhyuk3954@gmail.com>
2025-03-26 18:16:12 +0900

    1. feat: ReadStatusController update 담당 메소드 구현

--- src/main/java/com/sprint/mission/discodeit/Controller/ReadStatusController.java
+++ src/main/java/com/sprint/mission/discodeit/Controller/ReadStatusController.java
@@ -1,15 +1,16 @@
 package com.sprint.mission.discodeit.Controller;
 
 import com.sprint.mission.discodeit.dto.request.ReadStatusCreateRequest;
+import com.sprint.mission.discodeit.dto.request.ReadStatusUpdateRequest;
 import com.sprint.mission.discodeit.entity.ReadStatus;
 import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.service.ReadStatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/readstatus")
@@ -22,5 +23,15 @@ public class ReadStatusController {
         ReadStatus createReadStatus = readStatusService.create(readStatusCreateRequest);
         return ResponseEntity.ok(createReadStatus);
     }
+
+    @PutMapping("/{readStatusId}")
+    public ResponseEntity<ReadStatus> updateReadStatus(@PathVariable UUID readStatusId, @RequestBody ReadStatusUpdateRequest readStatusUpdateRequest) {
+        ReadStatus updateReadStatus = readStatusService.update(readStatusId, readStatusUpdateRequest);
+        return ResponseEntity.ok(updateReadStatus);
+    }
+
+
+
+
 
 }
