6eefe020736215b699d9eb2c66f1b2534016ce52
Minhyuk Sim <minhyuk3954@gmail.com>
2025-04-18 15:08:42 +0900

    refactor: binaryContentController 다운로드 api

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -1,11 +1,18 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.data.BinaryContentDto;
 import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.mapper.BinaryContentMapper;
 import com.sprint.mission.discodeit.service.BinaryContentService;
+import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.InputStream;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,6 +21,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class BinaryContentController {
     private final BinaryContentService binaryContentService;
+    private final BinaryContentStorage binaryContentStorage;
+    private final BinaryContentMapper binaryContentMapper;
 
     @GetMapping("/{binaryContentId}")
     public ResponseEntity<BinaryContent> find(@PathVariable("binaryContentId") UUID binaryContentId) {
@@ -24,6 +33,14 @@ public class BinaryContentController {
     public ResponseEntity<List<BinaryContent>> findAll(@RequestParam("binaryContentIds") List<UUID> binaryContentId) {
         List<BinaryContent> allbinary = binaryContentService.findAllByIdIn(binaryContentId);
         return ResponseEntity.ok(allbinary);
+    }
+
+    @GetMapping("/{binaryContentId}/download")
+    public ResponseEntity<?> download(@PathVariable UUID binaryContentId) {
+        BinaryContent binaryContent = binaryContentService.find(binaryContentId);
+        BinaryContentDto dto = binaryContentMapper.toDto(binaryContent);
+
+        return binaryContentStorage.download(dto);
     }
 
 }
