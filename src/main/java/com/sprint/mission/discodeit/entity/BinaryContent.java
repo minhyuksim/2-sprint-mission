8fb44e9e7a10cda634ae0844f75f76111b4e2e5b
Minhyuk Sim <minhyuk3954@gmail.com>
2025-04-18 11:09:49 +0900

    feat: binarycontent byte필드 삭제 및 storage인터페이스 설계

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -15,14 +15,12 @@ public class BinaryContent extends BaseEntity {
     private String fileName;
     private Long size;
     private String contentType;
-    private byte[] bytes;
 
     @Builder
-    public BinaryContent(String fileName, Long size, String contentType, byte[] bytes) {
+    public BinaryContent(String fileName, Long size, String contentType) {
         this.id = UUID.randomUUID();
         this.fileName = fileName;
         this.size = size;
         this.contentType = contentType;
-        this.bytes = bytes;
     }
 }
