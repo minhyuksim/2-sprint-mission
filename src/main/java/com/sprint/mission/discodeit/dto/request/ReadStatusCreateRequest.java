cc5222274ff5d2e8a6782b268b60add285aab0f4
Minhyuk Sim <minhyuk3954@gmail.com>
2025-05-12 15:10:16 +0900

    fix: blank추가

--- src/main/java/com/sprint/mission/discodeit/dto/request/ReadStatusCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/ReadStatusCreateRequest.java
@@ -1,11 +1,17 @@
 package com.sprint.mission.discodeit.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.Instant;
 import java.util.UUID;
 
 public record ReadStatusCreateRequest(
+    @NotNull(message = "유저ID는 필수입니다.")
     UUID userId,
+
+    @NotNull(message = "채널ID는 필수입니다.")
     UUID channelId,
+
     Instant lastReadAt
 ) {
 
--- src/main/java/com/sprint/mission/discodeit/dto/request/UserUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/UserUpdateRequest.java
@@ -5,15 +5,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record UserUpdateRequest(
-    @NotNull(message = "유저이름을 반드시 입력해야 합니다.")
-    @NotBlank(message = "이름에 공백이 있으면 안됩니다.")
+    @NotBlank(message = "유저이름을 반드시 입력해야 합니다.")
     String newUsername,
 
-    @NotNull(message = "이메일을 반드시 입력해야 합니다.")
+    @NotBlank(message = "이메일을 반드시 입력해야 합니다.")
     @Email(message = "유효한 이메일 양식이 아닙니다.")
     String newEmail,
 
-    @NotNull(message = "비밀번호를 반드시 입력해야 합니다.")
+    @NotBlank(message = "비밀번호를 반드시 입력해야 합니다.")
     String newPassword
 ) {
 
