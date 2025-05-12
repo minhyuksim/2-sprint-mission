4b45488880d701ee084b2125d8128ac36c345656
Minhyuk Sim <minhyuk3954@gmail.com>
2025-05-12 16:12:44 +0900

    feat: globalexception methodargumentnotvalidexception구현

--- src/main/java/com/sprint/mission/discodeit/exception/Basic/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/Basic/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.sprint.mission.discodeit.exception.Basic;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import com.sprint.mission.discodeit.dto.response.ErrorResponse;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -26,4 +31,30 @@ public class GlobalExceptionHandler {
         .status(error.getStatus())
         .body(re);
   }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    Map<String, Object> details = new HashMap<>();
+
+    ex.getBindingResult().getAllErrors().forEach(error -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      details.put(fieldName, errorMessage);
+    });
+
+    ErrorResponse re = ErrorResponse.builder()
+            .timestamp(Instant.now())
+            .code("VALIDATION_ERROR")
+            .message("Request validation failed")
+            .details(details)
+            .exceptionType(ex.getClass().getSimpleName())
+            .status(HttpStatus.BAD_REQUEST.value())
+            .build();
+
+    return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(re);
+  }
+
+
 }
